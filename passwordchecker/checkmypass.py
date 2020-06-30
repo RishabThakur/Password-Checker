cdd5a9d2450dc1e196f40193023c0b2749631e23
RishabThakur <67369880+RishabThakur@users.noreply.github.com>
2020-06-30 15:57:07 +0530

    Add files via upload

---
+++ passwordchecker/checkmypass.py
@@ -0,0 +1,44 @@
+# This project is answer for are your passwords ever been hacked?
+#if Yes! How many times?
+# so Lets Run it in command prompt( Windows ),or Gitbash wherever you want !in your terminal 
+import requests
+import hashlib
+import sys
+ 
+ # It requests the pwnedpasswords website.To access data
+def request_api_data(query_char):
+  url = 'https://api.pwnedpasswords.com/range/' + query_char
+  res = requests.get(url)
+  if res.status_code != 200:
+    raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
+  return res
+
+#  looping
+def get_password_leaks_count(hashes, hash_to_check):
+  hashes = (line.split(':') for line in hashes.text.splitlines())
+  for h, count in hashes:
+    if h == hash_to_check:
+      return count
+  return 0
+
+# converts our text psk to hashedpasswords
+def pwned_api_check(password):
+  sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+  first5_char, tail = sha1password[:5], sha1password[5:]
+  response = request_api_data(first5_char)
+  return get_password_leaks_count(response, tail)
+
+ #Getting count.... 
+
+def main(args):
+  for password in args:
+    count = pwned_api_check(password)
+    if count:
+      print(f'{password} was hacked! {count} times... you should probably change your password!')
+    else:
+      print(f'{password} was NOT found. Carry on!')
+  return 'done!'
+
+#Running the program
+if __name__ == '__main__':
+  sys.exit(main(sys.argv[1:]))
